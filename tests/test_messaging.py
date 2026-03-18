a25249c254ce9c137f952d09c5a5bf915c4afd11
di-ton <141556457+di-ton@users.noreply.github.com>
2026-03-18 19:28:32 +0200

    Add tests for messaging app

---
+++ tests/test_messaging.py
@@ -0,0 +1,98 @@
+from django.contrib.auth.models import Group
+from django.core.exceptions import PermissionDenied
+from django.test import TestCase
+from django.urls import reverse
+
+from accounts.choices import AcademicDegreeChoices
+from accounts.models import ScientistProfile, User
+from messaging.models import MessageRecipient, Message
+
+
+class SendMessageTests(TestCase):
+
+    def setUp(self):
+        self.sender = User.objects.create_user(email="sender@gmail.com", password="pass12345pass")
+        self.recipient_user = User.objects.create_user(email="recipient@gmail.com", password="pass123456")
+
+        Group.objects.create(name="Profile Moderators")
+
+        self.recipient_profile = ScientistProfile.objects.create(
+            user=self.recipient_user,
+            first_name="Ivan",
+            last_name="Dimitrov",
+            academic_degree=AcademicDegreeChoices.PHD,
+            affiliation="MU-Sofia",
+            orcid_id="0000-1111-0000-1111"
+        )
+
+    def test_send_message_creates_recipients(self):
+        self.client.login(email="sender@gmail.com", password="pass12345pass")
+
+        self.client.post(reverse("send-message", args=[self.recipient_profile.slug]), {
+            "subject": "Hello",
+            "body": "Test message"
+        })
+
+        self.assertEqual(MessageRecipient.objects.count(), 2)
+
+
+class InboxTests(TestCase):
+
+    def test_inbox_excludes_own_messages(self):
+        user = User.objects.create_user(email="user@gmail.com", password="pass12345pass")
+        message = Message.objects.create(sender=user, subject="Article", body="Let's submitted it today.")
+        MessageRecipient.objects.create(message=message, recipient=user)
+        self.client.login(email="user@gmail.com", password="pass12345pass")
+        response = self.client.get(reverse("inbox"))
+
+        self.assertEqual(len(response.context["messages"]), 0)
+
+
+class MessageDetailTests(TestCase):
+
+    def test_message_marked_as_read(self):
+        sender = User.objects.create_user(email="send@abv.bg", password="pass12345pass")
+        recipient = User.objects.create_user(email="recip@gmail.com", password="pass123456")
+        message = Message.objects.create(sender=sender, subject="Add members", body="There is a researcher who wants to join the project")
+
+        MessageRecipient.objects.create(message=message, recipient=recipient, is_read=False)
+
+        self.client.login(email="recip@gmail.com", password="pass123456")
+
+        self.client.get(reverse("message-detail", args=[message.pk]))
+
+        received_message = MessageRecipient.objects.get(message=message, recipient=recipient)
+        self.assertTrue(received_message.is_read)
+
+
+class MessagePermissionTests(TestCase):
+
+    def test_user_cannot_view_other_messages(self):
+        sender = User.objects.create_user(email="send@abv.bg", password="pass12345pass")
+        recipient = User.objects.create_user(email="recip@gmail.com", password="pass123456")
+        outsider = User.objects.create_user(email="out@gmail.com", password="pass123word")
+
+        message = Message.objects.create(sender=sender, subject="test subject", body="Test content")
+        MessageRecipient.objects.create(message=message, recipient=recipient)
+
+        self.client.login(email="out@gmail.com", password="pass123word")
+
+        response = self.client.get(reverse("message-detail", args=[message.pk]))
+
+        self.assertEqual(response.status_code, 403)
+
+
+class MessageModelTests(TestCase):
+
+    def test_is_project_message(self):
+        user = User.objects.create_user(email="user@gmail.com", password="pass12345pass")
+
+        message = Message.objects.create(
+            sender=user,
+            subject="test",
+            body="TestTestTestTestTest"
+        )
+
+        self.assertFalse(message.is_project_message)
+
+
